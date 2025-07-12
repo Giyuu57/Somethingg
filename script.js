f6de7f1bfead264428aba65a38e5c3d8066ff30b
Gauransh <gouranshsattavan@gmail.com>
2025-07-12 18:53:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', function() {
         videoFrame.style.display = 'block';
         
         // Set the YouTube embed URL with autoplay
-        youtubePlayer.src = 'https://www.youtube.com/embed/KIzCpTA2p5Y?autoplay=1';
+        youtubePlayer.src = 'https://www.youtube.com/watch?v=KIzCpTA2p5Y';
 
         
         // Add some visual feedback
